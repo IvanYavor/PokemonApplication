6c616779c4d42db1a1216890dfc58b39523583c2
Ivan Yavorksy <02ivan0971@gmail.com>
2023-04-15 15:27:24 +0300

    Read db url from env

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // TODO use env for db path
 mongoose
-  .connect("mongodb://127.0.0.1:27017/pokemons", { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URL, { useNewUrlParser: true })
   .catch((e) => {
     console.error("Connection error", e.message);
   });
